a79f9d02e9aab5f591f025ee7d6ef4420343b33e
Nasreen </>
2022-11-03 15:04:47 +0000

    Improve comments and update README

--- README.md
+++ README.md
@@ -1,2 +1,38 @@
 # Scallop
-A minimal toy shell written in Python
+
+This is a minimal toy shell written in Python.  It was written as a learning exercise.
+
+I wrote a few blog posts about it [here](https://nsreeen.github.io/tags/shell/).
+
+## Features
+Features implemented:
+- system calls (eg. ls, mkdir, etc.)
+- cd
+- `&&` command
+- pipes
+
+Sample:
+```
+/home/nasreen/projects/Scallop>> mkdir hello
+/home/nasreen/projects/Scallop>> cd hello
+/home/nasreen/projects/Scallop/hello>> cd ..
+/home/nasreen/projects/Scallop>> echo hi && echo bye
+hi
+bye
+/home/nasreen/projects/Scallop>> ls
+hello  README.md  scallop.py
+/home/nasreen/projects/Scallop>> ls | wc
+      3       3      27
+```
+
+## Usage
+To run from the root directory:
+`python3 scallop.py`
+
+## Resources
+The following resources helped me understand shells and write this:
+https://github.com/tokenrove/build-your-own-shell
+https://github.com/kamalmarhubi/shell-workshop
+https://github.com/bminor/bash
+https://www.aosabook.org/en/bash.html
+https://docs.python.org/3/library/os.html
--- scallop.py
+++ scallop.py
@@ -7,29 +7,30 @@ STDOUT_FILE_DESCRIPTOR_DEFAULT = 1
 
 
 def execute_pipe(pipe_1_tokens, pipe_2_tokens):
-        pipe_read, pipe_write = os.pipe()
+    # pipe gives us a temporay place to read and write intermediate steps of the pipe
+    pipe_read, pipe_write = os.pipe()
 
-        # pipe one writes to the pipe write fd rather than standard out
-        execute(pipe_1_tokens, fd_swap=(pipe_write, STDOUT_FILE_DESCRIPTOR_DEFAULT))
+    # pipe one writes to the pipe write fd rather than standard out
+    execute(pipe_1_tokens, file_descriptor_swap=(pipe_write, STDOUT_FILE_DESCRIPTOR_DEFAULT))
 
-        # we close the pipe write fd so that we can write to standard out again
-        os.close(pipe_write)
+    # we close the pipe write file so that we can write to standard out again
+    os.close(pipe_write)
 
-        # pipe two reads from pipe read (to read the output of the last step)
-        execute(pipe_2_tokens, fd_swap=(pipe_read, STDIN_FILE_DESCRIPTOR_DEFAULT))
+    # pipe two reads from pipe read (to read the output of the last step)
+    execute(pipe_2_tokens, file_descriptor_swap=(pipe_read, STDIN_FILE_DESCRIPTOR_DEFAULT))
 
 
-def execute(command_tokens, fd_swap=None):
+def execute(command_tokens, file_descriptor_swap=None):
     fork_return_value = os.fork()
     if fork_return_value == 0: # this is the child process
         try:
-            if fd_swap: # we redirect file descriptors to connect pipes
-                os.dup2(fd_swap[0], fd_swap[1])
+            if file_descriptor_swap: # we redirect file descriptors to connect pipes
+                os.dup2(file_descriptor_swap[0], file_descriptor_swap[1])
             os.execvp(command_tokens[0], command_tokens)
         except OSError as exception:
             print(f"Exception: {exception}")
 
-    else: # this is the parent process
+    else: # this is the parent process (fork returns the child pid in the parent process)
         child_result = os.waitpid(fork_return_value, 0)
 
 
@@ -46,7 +47,7 @@ def main():
 
             else:
                 command_tokens = command.split()
-                if command_tokens[0] == "cd": # we don't want to fork to execute cd
+                if command_tokens[0] == "cd": # we want to execute cd in the current process
                     os.chdir(os.path.abspath(command_tokens[1]))
 
                 else:
